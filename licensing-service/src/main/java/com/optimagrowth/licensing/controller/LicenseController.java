293115e4c2e608711f4854476f2643d754f37074
BeckZPL <beck.zhang@miraclechannel.ca>
2024-10-17 09:18:50 -0600

    project: licensing-service

---
+++ licensing-service/src/main/java/com/optimagrowth/licensing/controller/LicenseController.java
@@ -0,0 +1,9 @@
+package com.optimagrowth.licensing.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "organization/{organizationId}/license")
+public class LicenseController {
+}
